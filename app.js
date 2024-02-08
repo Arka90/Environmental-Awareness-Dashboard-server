a0ef3d5e92a309687a4fdf98f391a6cd832e2a8f
Arka90 <skarka90@gmail.com>
2024-02-08 12:28:29 +0530

    added cors policy for vercel

--- app.js
+++ app.js
@@ -6,7 +6,15 @@ const cors = require("cors");
 const weatherRouter = require("./routes/weatherRoutes");
 const app = express();
 app.use(express.json());
-app.use(cors());
+
+app.use(
+  cors({
+    origin: [
+      "https://environmental-awareness-dashboard-client.vercel.app",
+      "http://localhost:5173",
+    ],
+  })
+);
 app.use("/weather", weatherRouter);
 
 mongoose.connect(process.env.MONGO_URL).then(() => {
